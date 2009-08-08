92b66ba84eb912d7ae6dd2a5f855504c184c8990
Marcus Griep <marcus@griep.us>
2009-08-08 02:08:57 -0400

    Add basis for code contracts

---
+++ C5.Tests/TestAssembly.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.Contracts;
+using MbUnit.Framework;
+namespace C5.Tests
+{
+    [AssemblyFixture]
+    public class TestAssembly
+    {
+        [SetUp]
+        public static void AssemblyInitialize()
+        {
+            Contract.ContractFailed += (sender, e) =>
+                {
+                    e.SetHandled();
+                    e.SetUnwind(); // Forces abort after contract failure
+                    Assert.Fail("{0}:{1}", e.FailureKind, e.Message);
+                };
+        }
+    }
+}
