32043add94441dc1e234555231ab8f4be0a0d427
Marcus Griep <marcus@griep.us>
2009-08-08 01:27:31 -0400

    Add back in Pex dependency
    Allows for a better common merge base

---
+++ C5.Tests/Properties/PexAssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿// This file should only contain assembly level attributes.
+// Do not add any class or struct definitions as it may disable the ability of the tool to
+// update the file.
+
+using System;
+
+using System.Runtime.CompilerServices;
+using Microsoft.Pex.Framework.Coverage;
+using Microsoft.Pex.Framework.Instrumentation;
+using Microsoft.Pex.Framework.Settings;
+using Microsoft.Pex.Framework.Suppression;
+using Microsoft.Pex.Framework.Validation;
+using Microsoft.Pex.Graphs;
+using SCG = System.Collections.Generic;
+using C5;
+
+
+[assembly: InternalsVisibleTo("Microsoft.Pex, PublicKey=002400000480000094000000060200000024000052534131000400000100010057a289398cc79ca7e46cd08c3994ca4ec229dd91d5ab4047b1bfbef3833332beedad9bec4014cdf721177edb24fe644b0dd95ab436d262cac8640fc5a87082423be9f41db942c8d1fd622dc4b305424d4ce9baf88fd15735b7352a70587997681d7df9dd5635c5cec251fc84423ab0349538bf61ece4d8ee7a6f2d07d169b9ec")]
+
+[assembly: PexAssemblySettings(TestFramework = "MbUnitv3")]
+
+[assembly: PexGraphsPackage]
+
+[assembly: PexAssemblyUnderTest("C5")]
+[assembly: PexSuppressStaticFieldStore(typeof(Comparer<>), "cachedComparer")]
+[assembly: PexSuppressStaticFieldStore(typeof(SCG.Comparer<>), "defaultComparer")]
+[assembly: PexSuppressUninstrumentedMethodFromNamespace("System")]
+[assembly: PexSuppressUninstrumentedMethodFromNamespace("Microsoft")]
+
+[assembly: PexCoverageFilterAssembly(PexCoverageDomain.UserOrTestCode, "C5")]
+
+[assembly: PexAllowedContractRequiresFailureAtTypeUnderTestSurface]
---
+++ C5.Tests/SortingFixture.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Pex.Framework.Creatable;
+using SCG = System.Collections.Generic;
+using MbUnit.Framework;
+using Microsoft.Pex.Framework;
+using Microsoft.Pex.Framework.Validation;
+
+namespace C5.Tests
+{
+    [Author("Marcus Griep")]
+    [Category("Utility")]
+    [TestsOn(typeof(Sorting))]
+    [PexClass(typeof(Sorting), MaxConditions = 1000)]
+    [PexGenericArguments(typeof(int))]
+    public partial class SortingFixture
+    {
+        [PexMethod(MaxConditions = 4000)]
+        [Description("Constructs a specially designed array that triggers quicksort's worst-case running time.")]
+        // Examples: {1, 3, 2, 4} and {1, 5, 3, 7, 2, 4, 6, 8}
+        public void IntroSort_WorstCase(int k)
+        {
+            PexAssume.IsTrue(0 < k && k < int.MaxValue / 2 && k % 2 == 0);
+            var arr = new int[2 * k];
+
+            for (var i = 1; i <= k; ++i)
+            {
+                if (i % 2 == 0)
+                {
+                    arr[k + i - 1] = 2 * i;
+                    arr[k + i - 2] = 2 * i - 2;
+                }
+                else
+                {
+                    arr[i - 1] = i;
+                    arr[i] = i + k;
+                }
+            }
+
+            PexObserve.Value("array", arr);
+
+            IntroSort_Short(arr);
+        }
+
+        [PexMethod]
+        [PexAllowedExceptionFromTypeUnderTest(typeof(ArgumentNullException))]
+        public void IntroSort_Short<T>(T[] arr)
+        {
+            var sorted = arr == null ? null : (T[])arr.Clone();
+
+            Sorting.IntroSort(sorted);
+
+            PexObserve.Value("sorted", sorted);
+
+            AssertSorted(sorted, arr);
+        }
+
+        [PexMethod]
+        [PexAllowedExceptionFromTypeUnderTest(typeof(ArgumentNullException))]
+        [PexAllowedExceptionFromTypeUnderTest(typeof(ArgumentOutOfRangeException))]
+        public void IntroSort_Full<T>(T[] arr, int start, int count)
+        {
+            var sorted = arr == null ? null : (T[])arr.Clone();
+
+            Sorting.IntroSort(sorted, start, count, SCG.Comparer<T>.Default);
+
+            PexObserve.Value("sorted", sorted);
+
+            AssertSorted(sorted, arr, start, count);
+        }
+
+        [PexMethod]
+        [PexAllowedExceptionFromTypeUnderTest(typeof(ArgumentNullException))]
+        [PexAllowedExceptionFromTypeUnderTest(typeof(ArgumentOutOfRangeException))]
+        public void InsertionSort<T>(T[] arr, int start, int count)
+        {
+            var sorted = arr == null ? null : (T[])arr.Clone();
+
+            Sorting.InsertionSort(sorted, start, count, SCG.Comparer<T>.Default);
+
+            PexObserve.Value("sorted", sorted);
+
+            AssertSorted(sorted, arr, start, count);
+        }
+
+        [PexMethod]
+        [PexAllowedExceptionFromTypeUnderTest(typeof(ArgumentNullException))]
+        [PexAllowedExceptionFromTypeUnderTest(typeof(ArgumentOutOfRangeException))]
+        public void HeapSort<T>(T[] arr, int start, int count)
+        {
+            var sorted = arr == null ? null : (T[])arr.Clone();
+
+            Sorting.HeapSort(sorted, start, count, SCG.Comparer<T>.Default);
+
+            PexObserve.Value("sorted", sorted);
+
+            AssertSorted(sorted, arr, start, count);
+        }
+
+        internal static void AssertSorted<T>(T[] sorted, T[] arr)
+        {
+            AssertSorted(sorted, arr, 0, sorted.Length);
+        }
+
+        internal static void AssertSorted<T>(T[] sorted, T[] arr, int start, int count)
+        {
+            Assert.AreEqual(arr.Length, sorted.Length,
+                "Sorting algorithms should not change the length of the result.");
+
+            var comparison = new Comparison<T>(SCG.Comparer<T>.Default.Compare);
+            var eqalityComparer = SCG.EqualityComparer<T>.Default;
+
+            var i = 0;
+
+            for (; i < start; ++i)
+            {
+                Assert.AreEqual(arr[i], sorted[i], eqalityComparer,
+                    "Items outside the sort area should not be modified. Index: {0}", i);
+            }
+            for (++i; i < start + count; ++i)
+            {
+                Assert.LessThanOrEqualTo(sorted[i - 1], sorted[i], comparison,
+                    "Items in the sort area are not properly sorted. Index: {0}", i);
+            }
+            for (; i < sorted.Length; ++i)
+            {
+                Assert.AreEqual(arr[i], sorted[i], eqalityComparer,
+                    "Items outside the sort area should not be modified. Index: {0}", i);
+            }
+        }
+    }
+}
