10ce53a6fb7fdc099be6319b0f83064eb3d95026
Marcus Griep <marcus@griep.us>
2009-08-11 09:41:31 -0400

    Add a contract utility function class

---
+++ C5/Contracts/ContractUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace C5.Contracts
+{
+    internal sealed class Logical
+    {
+        [Pure]
+        public delegate bool Consequent();
+
+        [Pure]
+        public static bool Implication(bool p, Consequent q)
+        {
+            Contract.Ensures(Equivalence(Contract.Result<bool>(), () => !p || q()));
+
+            return !p || q();
+        }
+
+        [Pure]
+        public static bool Equivalence(bool p, Consequent q)
+        {
+            Contract.Ensures(Contract.Result<bool>() && p && q() ||
+                             !Contract.Result<bool>() && !p && !q());
+            //Contract.Ensures(Equivalence(proposition, equivalent));
+
+            return p && q() || !p && !q();
+        }
+    }
+}
